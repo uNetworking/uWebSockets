1531df47155c06d8b3e5b1665c6b63b275c7e41f
Alex Hultman <alexhultman@localhost.localdomain>
2016-04-18 23:33:08 +0200

    Reset echo.js to working state

--- nodejs/echo.js
+++ nodejs/echo.js
@@ -4,10 +4,6 @@ const WebSocketServer = require('./dist/uws').Server;
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
-  var typedArray = new Float32Array(10);
-  typedArray.fill(1234);
-  console.log(typedArray);
-  ws.close(1011, typedArray);
   ws.on('message', function incoming(message) {
     ws.send(message, { binary: Buffer.isBuffer(message) });
   });
