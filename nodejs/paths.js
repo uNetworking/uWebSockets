343ca33080c1d2c0134573f50cdc6d41ce0ecb76
Alex Hultman <alexhultman@localhost.localdomain>
2016-05-31 13:59:16 +0200

    Add paths.js example with multiple paths

--- nodejs/paths.js
+++ nodejs/paths.js
@@ -1,26 +1,26 @@
-/* echo server */
+// example showing usage with different behavior on a path basis
 
 const http = require('http');
+const WebSocketServer = require('./dist/uws').Server;
+
 const httpServer = http.createServer((request, response) => {
-  response.end('Hello');
+  response.end();
 });
 
-function verify (info) {
-  console.log(info.secure);
-  return true;
-}
-
-const WebSocketServer = require('./dist/uws').Server;
-const wss = new WebSocketServer({ path: 'echo', server: httpServer, verifyClient: verify });
-
-wss.on('connection', function connection(ws) {
+// echo server on path /echo, echoes the verbose message
+const wssEcho = new WebSocketServer({ path: 'echo', server: httpServer });
+wssEcho.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     ws.send(message, { binary: Buffer.isBuffer(message) });
   });
 });
 
-wss.on('error', function error(e) {
-  console.log('Error: ' + e);
+// scream echo server on path /scream, echoes the uppercase message
+const wssScream = new WebSocketServer({ path: 'scream', server: httpServer });
+wssScream.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    ws.send(message.toUpperCase(), { binary: Buffer.isBuffer(message) });
+  });
 });
 
 httpServer.listen(3000);
