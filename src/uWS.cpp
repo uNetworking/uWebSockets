bd1def4c0b5d2a3c8d99bd2b4f78e104f20c4c4e
Khaled BELAID <kbelaid@ivsweb.com>
2016-06-27 16:22:06 +0200

    force socket closing to prevent infinite loop

--- src/uWS.cpp
+++ src/uWS.cpp
@@ -1203,6 +1203,8 @@ void Socket::write(char *data, size_t length, bool transferOwnership, void(*call
 
                 if (socketData->state == CLOSING) {
                     cout << "CLOSING state, Socket::write" << endl;
+                    // force close in order to prevent infinite loop.
+                    Socket(handle).close(true, 1006);
                     return;
                 }
 
