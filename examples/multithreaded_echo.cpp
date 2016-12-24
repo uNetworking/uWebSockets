d58940d122f4ddb9cf9de19d612b183eb6f5882b
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-12-24 16:53:45 -0500

    Fixed multithreading example
    uWS::UpgradeInfo doesn't exist.
    
    Also move NodeData::addAsync() call to after Node::listen() check,
    to avoid assertion in libuv's uv_loop_delete.

--- examples/multithreaded_echo.cpp
+++ examples/multithreaded_echo.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 using namespace uWS;
@@ -9,48 +11,59 @@ using namespace uWS;
 #define THREADS 4
 uWS::Hub *threadedServer[THREADS];
 
+static string hexDump(char *buf, size_t size);
 
 int main()
 {
-	try {
-		// you need at least one server listening to a port
-		Hub h;
-		
-		h.onConnection([&](uWS::WebSocket<uWS::SERVER> ws, uWS::UpgradeInfo upgInfo){
-			int t = rand() % THREADS;
-			cout << "Transfering connection to thread " << t << endl;
-			ws.transfer(&threadedServer[t]->getDefaultGroup<uWS::SERVER>());
-		});
-		
-		
-		// launch the threads with their servers
-		for (int i = 0; i < THREADS; i++) {
-			new thread([i]{
-				// register our events
-				threadedServer[i] = new Hub();
-				
-				threadedServer[i]->onDisconnection([&i](WebSocket<uWS::SERVER> ws, int code, char *message, size_t length){
-					cout << "Disconnection on thread " << i << endl;
-				});
-				
-				threadedServer[i]->onMessage([&i](uWS::WebSocket<uWS::SERVER> ws, char *message, size_t length, uWS::OpCode code){
-					cout << "Message on thread " << i << ": " << string(message, length) << endl;
-					ws.send((char *) message, length, code);
-				});
-				
-				threadedServer[i]->getDefaultGroup<uWS::SERVER>().addAsync();
-				threadedServer[i]->run();
+	// you need at least one server listening to a port
+	Hub h;
+	
+	h.onConnection([&](uWS::WebSocket<uWS::SERVER> ws, uWS::HTTPRequest httpReq){
+		int t = rand() % THREADS;
+		cout << "Transferring connection to thread " << t << endl;
+		ws.transfer(&threadedServer[t]->getDefaultGroup<uWS::SERVER>());
+	});
+	
+	
+	// launch the threads with their servers
+	for (int i = 0; i < THREADS; i++) {
+		new thread([i]{
+			// register our events
+			threadedServer[i] = new Hub();
+			
+			threadedServer[i]->onDisconnection([&i](WebSocket<uWS::SERVER> ws, int code, char *message, size_t length){
+				cout << "Disconnection on thread " << i << endl;
 			});
 			
-		}
+			threadedServer[i]->onMessage([&i](uWS::WebSocket<uWS::SERVER> ws, char *message, size_t length, uWS::OpCode code){
+				//cerr << "Message on thread " << i << ": " << hexDump(message, length) << endl;
+				ws.send((char *) message, length, code);
+			});
+			
+			threadedServer[i]->getDefaultGroup<uWS::SERVER>().addAsync();
+			threadedServer[i]->run();
+		});
 		
-		h.getDefaultGroup<uWS::SERVER>().addAsync();
-		h.listen(3000);
-		h.run();
-		
-	} catch (...) {
-		cout << "ERR_LISTEN" << endl;
+	}
+	
+    if (h.listen(3000)) {
+	    h.getDefaultGroup<uWS::SERVER>().addAsync();
+        h.run();
+	} else {
+		cerr << "ERR_LISTEN" << endl;
 	}
 	
 	return 0;
+}
+
+string hexDump(char *buf, size_t size) {
+    if (buf == nullptr) {
+        return "";
+    }
+    static const char hexChars[] = "0123456789abcdef";
+    stringstream ss;
+    for (size_t i = 0; i < size; i++) {
+        ss << "\\x" << hexChars[(buf[i] >> 4) & 0xf] << hexChars[buf[i] & 0xf];
+    }
+    return ss.str();
 }
