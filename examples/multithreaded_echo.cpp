e45f2b10574dd74bc8480b1f464273cb63e9f2d2
Yixin Zhang <m.yixin.zhang@gmail.com>
2017-01-12 12:16:39 -0800

    Removed output in onMessage handler
    Affects performance and clutters the screen.

--- examples/multithreaded_echo.cpp
+++ examples/multithreaded_echo.cpp
@@ -1,69 +1,47 @@
 #include <uWS.h>
 #include <iostream>
-#include <string>
 #include <thread>
-#include <sstream>
-#include <iomanip>
-using namespace std;
-
-using namespace uWS;
 
 #define THREADS 4
 uWS::Hub *threadedServer[THREADS];
 
-static string hexDump(char *buf, size_t size);
-
 int main()
 {
 	// you need at least one server listening to a port
-	Hub h;
-	
+	uWS::Hub h;
+
 	h.onConnection([&](uWS::WebSocket<uWS::SERVER> ws, uWS::HTTPRequest httpReq){
 		int t = rand() % THREADS;
-		cout << "Transferring connection to thread " << t << endl;
+		std::cout << "Transferring connection to thread " << t << std::endl;
 		ws.transfer(&threadedServer[t]->getDefaultGroup<uWS::SERVER>());
 	});
-	
-	
+
 	// launch the threads with their servers
 	for (int i = 0; i < THREADS; i++) {
-		new thread([i]{
+		new std::thread([i]{
 			// register our events
-			threadedServer[i] = new Hub();
-			
-			threadedServer[i]->onDisconnection([&i](WebSocket<uWS::SERVER> ws, int code, char *message, size_t length){
-				cout << "Disconnection on thread " << i << endl;
+			threadedServer[i] = new uWS::Hub();
+
+			threadedServer[i]->onDisconnection([&i](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length){
+				std::cout << "Disconnection on thread " << i << std::endl;
 			});
-			
+
 			threadedServer[i]->onMessage([&i](uWS::WebSocket<uWS::SERVER> ws, char *message, size_t length, uWS::OpCode code){
-				//cerr << "Message on thread " << i << ": " << hexDump(message, length) << endl;
 				ws.send((char *) message, length, code);
 			});
-			
+
 			threadedServer[i]->getDefaultGroup<uWS::SERVER>().addAsync();
 			threadedServer[i]->run();
 		});
-		
+
 	}
-	
+
     if (h.listen(3000)) {
 	    h.getDefaultGroup<uWS::SERVER>().addAsync();
         h.run();
 	} else {
-		cerr << "ERR_LISTEN" << endl;
+		std::cerr << "ERR_LISTEN" << std::endl;
 	}
-	
-	return 0;
-}
 
-string hexDump(char *buf, size_t size) {
-    if (buf == nullptr) {
-        return "";
-    }
-    static const char hexChars[] = "0123456789abcdef";
-    stringstream ss;
-    for (size_t i = 0; i < size; i++) {
-        ss << "\\x" << hexChars[(buf[i] >> 4) & 0xf] << hexChars[buf[i] & 0xf];
-    }
-    return ss.str();
+	return 0;
 }
