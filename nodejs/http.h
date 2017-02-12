82af54d08a8d843ac834dea5697aabd76f153175
Alex Hultman <alexhultman@localhost.localdomain>
2017-02-12 22:32:45 +0100

    Node.js: Structure http.h up nicely

--- nodejs/http.h
+++ nodejs/http.h
@@ -1,123 +1,136 @@
-uWS::HttpRequest currentReq;
 Persistent<Object> reqTemplate, resTemplate;
 Persistent<Function> httpPersistent;
-
-/*
-void onHttpUpgrade(const FunctionCallbackInfo<Value> &args) {
-    uWS::Group<uWS::SERVER> *group = (uWS::Group<uWS::SERVER> *) args[0].As<External>()->Value();
-    GroupData *groupData = (GroupData *) group->getUserData();
-
-    Isolate *isolate = args.GetIsolate();
-    Persistent<Function> *httpUpgradeCallback = &groupData->httpUpgradeHandler;
-    httpUpgradeCallback->Reset(isolate, Local<Function>::Cast(args[1]));
-    group->onHttpUpgrade([isolate, httpUpgradeCallback](uWS::HttpSocket<uWS::SERVER> s, uWS::HttpRequest req) {
-        currentReq = req;
-        HandleScope hs(isolate);
-        Local<Value> argv[] = {External::New(isolate, s.getPollHandle()),
-                               Integer::New(isolate, req.getMethod()),
-                               String::NewFromUtf8(isolate, req.getUrl().value, String::kNormalString, req.getUrl().valueLength)};
-        Local<Function>::New(isolate, *httpUpgradeCallback)->Call(isolate->GetCurrentContext()->Global(), 3, argv);
-    });
-}*/
-
-void reqOn(const FunctionCallbackInfo<Value> &args) {
-    NativeString eventName(args[0]);
-    if (std::string(eventName.getData(), eventName.getLength()) == "data") {
-        args.Holder()->SetAlignedPointerInInternalField(1, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
-    } else if (std::string(eventName.getData(), eventName.getLength()) == "end") {
-        args.Holder()->SetAlignedPointerInInternalField(2, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
-    }
-    args.GetReturnValue().Set(args.Holder());
-}
-
-void resOn(const FunctionCallbackInfo<Value> &args) {
-    NativeString eventName(args[0]);
-    if (std::string(eventName.getData(), eventName.getLength()) == "close") {
-        args.Holder()->SetAlignedPointerInInternalField(1, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
-    }
-    args.GetReturnValue().Set(args.Holder());
-}
-
-void reqGetHeader(Local<String> property, const PropertyCallbackInfo<Value> &args) {
-    NativeString nativeString(property);
-    uWS::Header header = currentReq.getHeader(nativeString.getData(), nativeString.getLength());
-    if (header) {
-        args.GetReturnValue().Set(String::NewFromUtf8(args.GetIsolate(), header.value, String::kNormalString, header.valueLength));
-    }
-}
-
-void reqUrl(Local<String> property, const PropertyCallbackInfo<Value> &args) {
-    args.GetReturnValue().Set(String::NewFromOneByte(args.GetIsolate(), (uint8_t *) currentReq.getUrl().value, String::kNormalString, currentReq.getUrl().valueLength));
-}
-
-void reqMethod(Local<String> property, const PropertyCallbackInfo<Value> &args) {
-    for (int i = 0; i < currentReq.getUrl().keyLength; i++) {
-        currentReq.getUrl().key[i] &= ~32;
-    }
-    args.GetReturnValue().Set(String::NewFromOneByte(args.GetIsolate(), (uint8_t *) currentReq.getUrl().key, String::kNormalString, currentReq.getUrl().keyLength));
-}
-
-void resEnd(const FunctionCallbackInfo<Value> &args) {
-    uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
-
-    if (res) {
-        NativeString nativeString(args[0]);
-
-        ((Persistent<Value> *) &res->userData)->Reset();
-        ((Persistent<Value> *) &res->userData)->~Persistent<Value>();
-        ((Persistent<Value> *) &res->extraUserData)->Reset();
-        ((Persistent<Value> *) &res->extraUserData)->~Persistent<Value>();
-        res->end(nativeString.getData(), nativeString.getLength());
-    }
-}
-
-void resWriteHead(const FunctionCallbackInfo<Value> &args) {
-    uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
-
-    if (res) {
-        std::string head = "HTTP/1.1 " + std::to_string(args[0]->IntegerValue()) + " ";
-
-        Local<Object> headersObject;
-        if (args[1]->IsString()) {
-            NativeString statusMessage(args[1]);
-            head.append(statusMessage.getData(), statusMessage.getLength());
-            headersObject = args[2]->ToObject();
-        } else {
-            head += "OK";
-            headersObject = args[1]->ToObject();
-        }
-
-        Local<Array> headers = headersObject->GetOwnPropertyNames();
-        for (int i = 0; i < headers->Length(); i++) {
-            Local<Value> key = headers->Get(i);
-            Local<Value> value = headersObject->Get(key);
-
-            NativeString nativeKey(key);
-            NativeString nativeValue(value);
-
-            head += "\r\n";
-            head.append(nativeKey.getData(), nativeKey.getLength());
-            head += ": ";
-            head.append(nativeValue.getData(), nativeValue.getLength());
-        }
-
-        head += "\r\n\r\n";
-        res->write(head.data(), head.length());
-    }
-}
-
-// todo: if not writeHead called before then should write implicit headers
-void resWrite(const FunctionCallbackInfo<Value> &args) {
-    uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
-
-    if (res) {
-        NativeString nativeString(args[0]);
-        res->write(nativeString.getData(), nativeString.getLength());
-    }
-}
+uWS::HttpRequest currentReq;
 
 struct HttpServer {
 
+    struct Request {
+        static void on(const FunctionCallbackInfo<Value> &args) {
+            NativeString eventName(args[0]);
+            if (std::string(eventName.getData(), eventName.getLength()) == "data") {
+                args.Holder()->SetAlignedPointerInInternalField(1, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
+            } else if (std::string(eventName.getData(), eventName.getLength()) == "end") {
+                args.Holder()->SetAlignedPointerInInternalField(2, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
+            }
+            args.GetReturnValue().Set(args.Holder());
+        }
+
+        static void headers(Local<String> property, const PropertyCallbackInfo<Value> &args) {
+            NativeString nativeString(property);
+            uWS::Header header = currentReq.getHeader(nativeString.getData(), nativeString.getLength());
+            if (header) {
+                args.GetReturnValue().Set(String::NewFromOneByte(args.GetIsolate(), (uint8_t *) header.value, String::kNormalString, header.valueLength));
+            }
+        }
+
+        static void url(Local<String> property, const PropertyCallbackInfo<Value> &args) {
+            args.GetReturnValue().Set(String::NewFromOneByte(args.GetIsolate(), (uint8_t *) currentReq.getUrl().value, String::kNormalString, currentReq.getUrl().valueLength));
+        }
+
+        static void method(Local<String> property, const PropertyCallbackInfo<Value> &args) {
+            for (int i = 0; i < currentReq.getUrl().keyLength; i++) {
+                currentReq.getUrl().key[i] &= ~32;
+            }
+            args.GetReturnValue().Set(String::NewFromOneByte(args.GetIsolate(), (uint8_t *) currentReq.getUrl().key, String::kNormalString, currentReq.getUrl().keyLength));
+        }
+
+        static Local<Object> getTemplateObject(Isolate *isolate) {
+            Local<ObjectTemplate> headersTemplate = ObjectTemplate::New(isolate);
+            headersTemplate->SetNamedPropertyHandler(Request::headers);
+
+            // reqObject has room for 5 pointers - on('data', 'end') most important
+            Local<FunctionTemplate> reqTemplateLocal = FunctionTemplate::New(isolate);
+            reqTemplateLocal->InstanceTemplate()->SetInternalFieldCount(5);
+            reqTemplateLocal->PrototypeTemplate()->SetAccessor(String::NewFromUtf8(isolate, "url"), Request::url);
+            reqTemplateLocal->PrototypeTemplate()->SetAccessor(String::NewFromUtf8(isolate, "method"), Request::method);
+            reqTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "on"), FunctionTemplate::New(isolate, Request::on));
+            Local<Object> reqObjectLocal = reqTemplateLocal->GetFunction()->NewInstance();
+            reqObjectLocal->Set(String::NewFromUtf8(isolate, "headers"), headersTemplate->NewInstance());
+            return reqObjectLocal;
+        }
+    };
+
+    struct Response {
+        static void on(const FunctionCallbackInfo<Value> &args) {
+            NativeString eventName(args[0]);
+            if (std::string(eventName.getData(), eventName.getLength()) == "close") {
+                args.Holder()->SetAlignedPointerInInternalField(1, new Persistent<Function>(args.GetIsolate(), Local<Function>::Cast(args[1])));
+            }
+            args.GetReturnValue().Set(args.Holder());
+        }
+
+        static void end(const FunctionCallbackInfo<Value> &args) {
+            uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
+
+            if (res) {
+                NativeString nativeString(args[0]);
+
+                ((Persistent<Value> *) &res->userData)->Reset();
+                ((Persistent<Value> *) &res->userData)->~Persistent<Value>();
+                ((Persistent<Value> *) &res->extraUserData)->Reset();
+                ((Persistent<Value> *) &res->extraUserData)->~Persistent<Value>();
+                res->end(nativeString.getData(), nativeString.getLength());
+            }
+        }
+
+        // todo: this is slow
+        static void writeHead(const FunctionCallbackInfo<Value> &args) {
+            uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
+
+            if (res) {
+                std::string head = "HTTP/1.1 " + std::to_string(args[0]->IntegerValue()) + " ";
+
+                Local<Object> headersObject;
+                if (args[1]->IsString()) {
+                    NativeString statusMessage(args[1]);
+                    head.append(statusMessage.getData(), statusMessage.getLength());
+                    headersObject = args[2]->ToObject();
+                } else {
+                    head += "OK";
+                    headersObject = args[1]->ToObject();
+                }
+
+                Local<Array> headers = headersObject->GetOwnPropertyNames();
+                for (int i = 0; i < headers->Length(); i++) {
+                    Local<Value> key = headers->Get(i);
+                    Local<Value> value = headersObject->Get(key);
+
+                    NativeString nativeKey(key);
+                    NativeString nativeValue(value);
+
+                    head += "\r\n";
+                    head.append(nativeKey.getData(), nativeKey.getLength());
+                    head += ": ";
+                    head.append(nativeValue.getData(), nativeValue.getLength());
+                }
+
+                head += "\r\n\r\n";
+                res->write(head.data(), head.length());
+            }
+        }
+
+        // todo: if not writeHead called before then should write implicit headers
+        static void write(const FunctionCallbackInfo<Value> &args) {
+            uWS::HttpResponse *res = (uWS::HttpResponse *) args.Holder()->GetAlignedPointerFromInternalField(0);
+
+            if (res) {
+                NativeString nativeString(args[0]);
+                res->write(nativeString.getData(), nativeString.getLength());
+            }
+        }
+
+        static Local<Object> getTemplateObject(Isolate *isolate) {
+            Local<FunctionTemplate> resTemplateLocal = FunctionTemplate::New(isolate);
+            resTemplateLocal->InstanceTemplate()->SetInternalFieldCount(5);
+            resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "end"), FunctionTemplate::New(isolate, Response::end));
+            resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "writeHead"), FunctionTemplate::New(isolate, Response::writeHead));
+            resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "write"), FunctionTemplate::New(isolate, Response::write));
+            resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "on"), FunctionTemplate::New(isolate, Response::on));
+            return resTemplateLocal->GetFunction()->NewInstance();
+        }
+    };
+
+    // todo: wrap everything up - most important function to get correct
     static void createServer(const FunctionCallbackInfo<Value> &args) {
 
         if (sizeof(Persistent<Object>) != sizeof(void *)) {
@@ -215,46 +228,16 @@ struct HttpServer {
 
     static Local<Function> getHttpServer(Isolate *isolate) {
         Local<FunctionTemplate> httpServer = FunctionTemplate::New(isolate, HttpServer::createServer);
+        httpServer->InstanceTemplate()->SetInternalFieldCount(1);
 
         httpServer->Set(String::NewFromUtf8(isolate, "createServer"), FunctionTemplate::New(isolate, HttpServer::createServer));
         httpServer->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "listen"), FunctionTemplate::New(isolate, HttpServer::listen));
-        httpServer->InstanceTemplate()->SetInternalFieldCount(1);
 
 
         // on('upgrade') needed to integrate with uws
 
-
-
-
-
-
-        Local<ObjectTemplate> headersTemplate = ObjectTemplate::New(isolate);
-        headersTemplate->SetNamedPropertyHandler(reqGetHeader);
-
-        // reqObject has room for 5 pointers - on('data', 'end') most important
-        Local<FunctionTemplate> reqTemplateLocal = FunctionTemplate::New(isolate);
-        reqTemplateLocal->InstanceTemplate()->SetInternalFieldCount(5);
-        reqTemplateLocal->PrototypeTemplate()->SetAccessor(String::NewFromUtf8(isolate, "url"), reqUrl);
-        reqTemplateLocal->PrototypeTemplate()->SetAccessor(String::NewFromUtf8(isolate, "method"), reqMethod);
-        reqTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "on"), FunctionTemplate::New(isolate, reqOn));
-        Local<Object> reqObjectLocal = reqTemplateLocal->GetFunction()->NewInstance();
-        reqObjectLocal->Set(String::NewFromUtf8(isolate, "headers"), headersTemplate->NewInstance());
-        reqTemplate.Reset(isolate, reqObjectLocal);
-
-        // resObject has room for 5 pointers
-        Local<FunctionTemplate> resTemplateLocal = FunctionTemplate::New(isolate);
-        resTemplateLocal->InstanceTemplate()->SetInternalFieldCount(5);
-        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "end"), FunctionTemplate::New(isolate, resEnd));
-        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "writeHead"), FunctionTemplate::New(isolate, resWriteHead));
-        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "write"), FunctionTemplate::New(isolate, resWrite));
-        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "on"), FunctionTemplate::New(isolate, resOn));
-        resTemplate.Reset(isolate, resTemplateLocal->GetFunction()->NewInstance());
-
-
-
-
-
-
+        reqTemplate.Reset(isolate, Request::getTemplateObject(isolate));
+        resTemplate.Reset(isolate, Response::getTemplateObject(isolate));
 
         Local<Function> httpServerLocal = httpServer->GetFunction();
         httpPersistent.Reset(isolate, httpServerLocal);
