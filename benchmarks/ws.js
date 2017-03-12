4646e32e47fb18a9edcf605a7468128ea55f9182
Alex Hultman <alexhultman@localhost.localdomain>
2017-03-12 19:29:06 +0100

    Use shared functions in ws.js & socketio.js

--- benchmarks/ws.js
+++ benchmarks/ws.js
@@ -1,8 +1,11 @@
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 3000 });
 
+function incoming(message) {
+    this.send(message, {binary: true});
+}
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    ws.send(message, {binary: true});
-  });
+  //ws.upgradeReq = null;
+  ws.on('message', incoming);
 });
