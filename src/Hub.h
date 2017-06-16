b00980d4c50df010858b22303f97ce3d5bec7609
Michael B. Mathews <mike.mathews@comtechtel.com>
2017-06-16 15:04:52 -0700

    Refactored HTTP client functions from HubExtended class into hub.  More consolidation is possible

--- src/Hub.h
+++ src/Hub.h
@@ -10,6 +10,8 @@
 
 namespace uWS {
 
+using HttpHeaderMap = std::map<std::string, std::string>;
+
 struct  Hub : protected uS::Node, public Group<SERVER>, public Group<CLIENT> {
 protected:
     struct ConnectionData {
@@ -24,9 +26,12 @@ protected:
     std::string dynamicInflationBuffer;
     static const int LARGE_BUFFER_SIZE = 300 * 1024;
 
-    static void onServerAccept(uS::Socket *s);
-    static void onClientConnection(uS::Socket *s, bool error);
-	static uS::Socket *allocateHttpSocket(uS::Socket *s);
+	WIN32_EXPORT static void onServerAccept(uS::Socket *s);
+	WIN32_EXPORT static void onClientConnection(uS::Socket *s, bool error);
+	WIN32_EXPORT static uS::Socket *allocateHttpSocket(uS::Socket *s);
+	WIN32_EXPORT static void ontHttpOnlyConnection(uS::Socket *s, bool error);
+	WIN32_EXPORT HttpSocket<CLIENT>* HttpSocketConnect(const std::string& uri, void *user, Group<CLIENT> *eh, std::string& hostname, std::string& path);
+
 public:
     template <bool isServer>
     Group<isServer> *createGroup(int extensionOptions = 0, unsigned int maxPayload = 16777216) {
@@ -40,8 +45,21 @@ public:
 
 	WIN32_EXPORT bool listen(int port, uS::TLS::Context sslContext = nullptr, int options = 0, Group<SERVER> *eh = nullptr);
 	WIN32_EXPORT bool listen(const char *host, int port, uS::TLS::Context sslContext = nullptr, int options = 0, Group<SERVER> *eh = nullptr);
-	WIN32_EXPORT void connect(const std::string& uri, void *user = nullptr, const std::map<std::string, std::string>& extraHeaders = {}, int timeoutMs = 5000, Group<CLIENT> *eh = nullptr);
+
+	/**
+	* Connects using the web socket protocol with upgrade.  
+	* todo - rename connectWS
+	*/
+	WIN32_EXPORT void connect(const std::string& uri, void *user = nullptr, const HttpHeaderMap&  extraHeaders = {}, int timeoutMs = 5000, Group<CLIENT> *eh = nullptr);
+
 	WIN32_EXPORT void upgrade(uv_os_sock_t fd, const char *secKey, SSL *ssl, const char *extensions, size_t extensionsLength, const char *subprotocol, size_t subprotocolLength, Group<SERVER> *serverGroup = nullptr);
+
+	/**
+	* Makes a standard HTTP connection.
+	*/
+	WIN32_EXPORT void connect(const std::string& uri, HttpMethod method, void *user = nullptr, const HttpHeaderMap& extraHeaders = {},
+		const char* content = nullptr, size_t contentlength = 0, int timeoutMs = 5000, Group<CLIENT> *eh = nullptr);
+
 
     Hub(int extensionOptions = 0, bool useDefaultLoop = false, unsigned int maxPayload = 16777216) : uS::Node(LARGE_BUFFER_SIZE, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_PRE_PADDING, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_POST_PADDING, useDefaultLoop),
                                              Group<SERVER>(extensionOptions, maxPayload, this, nodeData), Group<CLIENT>(0, maxPayload, this, nodeData) {
