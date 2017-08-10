uNetworking/uWebSockets

commit 6435af4251575686c89248197ff21072bb7e44c8

Add hostname parsing for IPv6 addresses to Hub::connect

src/Hub.cpp
@@ -92,7 +92,13 @@ void Hub::connect(std::string uri, void *user, std::map<std::string, std::string
     size_t offset = 0;
     std::string protocol = uri.substr(offset, uri.find("://")), hostname, portStr, path;
     if ((offset += protocol.length() + 3) < uri.length()) {
-        hostname = uri.substr(offset, uri.find_first_of(":/", offset) - offset);
+        if (uri[offset] == "[") {
+            offset++;
+            hostname = uri.substr(offset, uri.find_first_of("]", offset) - offset);
+            offset++;
+        } else {
+            hostname = uri.substr(offset, uri.find_first_of(":/", offset) - offset);
+        }
 
         offset += hostname.length();
         if (uri[offset] == ':') {

commit fb137cbbcb5be1dd85d63c5babfcd73f980da1d9

Fix string comparison bug introduced in 6435af4251575686c89248197ff21072bb7e44c8

src/Hub.cpp
@@ -92,7 +92,7 @@ void Hub::connect(std::string uri, void *user, std::map<std::string, std::string
     size_t offset = 0;
     std::string protocol = uri.substr(offset, uri.find("://")), hostname, portStr, path;
     if ((offset += protocol.length() + 3) < uri.length()) {
-        if (uri[offset] == "[") {
+        if (uri[offset] == '[') {
             offset++;
             hostname = uri.substr(offset, uri.find_first_of("]", offset) - offset);
             offset++;