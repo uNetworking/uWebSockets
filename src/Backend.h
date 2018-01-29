eef818ce758a32e87cbd101e82add8d7bbc32de9
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-01-29 18:51:31 +0100

    make ifdef logic in Backend.h more readable

--- src/Backend.h
+++ src/Backend.h
@@ -3,15 +3,15 @@
 
 // Default to Epoll if nothing specified and on Linux
 // Default to Libuv if nothing specified and not on Linux
-#ifdef USE_ASIO
-#include "Asio.h"
-#elif !defined(__linux__) || defined(USE_LIBUV)
-#include "Libuv.h"
-#else
-#ifndef USE_EPOLL
-#define USE_EPOLL
-#endif
-#include "Epoll.h"
-#endif
+# ifdef USE_ASIO
+#   include "Asio.h"
+# elif !defined(__linux__) || defined(USE_LIBUV)
+#   include "Libuv.h"
+# else
+#   ifndef USE_EPOLL
+#     define USE_EPOLL
+#   endif
+#   include "Epoll.h"
+# endif
 
 #endif // BACKEND_H
