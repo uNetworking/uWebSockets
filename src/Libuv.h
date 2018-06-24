02e72db052ace7a6bdefa46454c86f5402fa1c16
Chaoran Yang <chaorany@me.com>
2018-06-24 05:03:04 +0000

    suppress uninitialized warning

--- src/Libuv.h
+++ src/Libuv.h
@@ -102,6 +102,7 @@ struct Poll {
     Poll(Loop *loop, uv_os_sock_t fd) {
         uv_poll = new uv_poll_t;
         uv_poll_init_socket(loop, uv_poll, fd);
+        cb = nullptr;
     }
 
     Poll(Poll &&other) {
