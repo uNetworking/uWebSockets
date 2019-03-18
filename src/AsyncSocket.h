03ff8277eac932efd02bca0cf715c8bda61d45f2
John <johnli1210@hotmail.com>
2019-03-18 10:49:43 +0800

    fix bug: protected member method cannot be call by object

--- src/AsyncSocket.h
+++ src/AsyncSocket.h
@@ -30,7 +30,7 @@ struct AsyncSocket {
     template <bool> friend struct HttpContext;
     template <bool, bool> friend struct WebSocketContext;
     friend class TopicTree;
-protected:
+//protected:
 
     /* Get loop data for socket */
     LoopData *getLoopData() {
