2c85dc51c63fcaba871908d7a0431cf458b54df4
Alex Hultman <alexhultman@gmail.com>
2019-06-10 00:16:30 +0200

    Use LLVMFuzzerInitialize instead

--- fuzzing/MockedHelloWorld.cpp
+++ fuzzing/MockedHelloWorld.cpp
@@ -5,14 +5,20 @@
 /* This function pushes data to the uSockets mock */
 extern void us_loop_read_mocked_data(struct us_loop *loop, char *data, unsigned int size);
 
-/* HelloWorld */
-auto app = uWS::App().get("/*", [](auto *res, auto *req) {
-	res->end("Hello world!");
-}).listen(9001, [](auto *token) {
-    if (token) {
-        std::cout << "Listening on port " << 9001 << std::endl;
-    }
-});
+uWS::TemplatedApp<false> *app;
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
+
+    app = new uWS::TemplatedApp<false>(uWS::App().get("/*", [](auto *res, auto *req) {
+        res->end("Hello world!");
+    }).listen(9001, [](auto *token) {
+        if (token) {
+            std::cout << "Listening on port " << 9001 << std::endl;
+        }
+    }));
+
+    return 0;
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
