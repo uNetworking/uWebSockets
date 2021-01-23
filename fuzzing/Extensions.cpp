e973bb41beb30ad7b5ac99ac05746a6e45ebef84
Alex Hultman <alexhultman@gmail.com>
2021-01-23 19:08:14 +0100

    Add extensions negotiation fuzzing

--- fuzzing/Extensions.cpp
+++ fuzzing/Extensions.cpp
@@ -4,34 +4,57 @@
 
 #include <cstdio>
 #include <string>
+#include <cstdlib>
 
 /* We test the websocket extensions parser */
 #include "../src/WebSocketExtensions.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
+    /* This one must not return shared compressor, or above 13 */
     {
-        uWS::ExtensionsNegotiator<true> extensionsNegotiator(uWS::Options::PERMESSAGE_DEFLATE);
-        extensionsNegotiator.readOffer({(char *) data, size});
+        auto [negCompression, negCompressionWindow, negInflationWindow, response] = uWS::negotiateCompression(true, 13, 0, std::string_view((char *) data, size));
 
-        extensionsNegotiator.generateOffer();
-        extensionsNegotiator.getNegotiatedOptions();
+        if (negCompression) {
+            /* If we want dedicated compression, we must not end up here! */
+            if (negCompressionWindow == 0) {
+                free((void *) -1);
+            }
+
+            if (negCompressionWindow > 13) {
+                free((void *) -1);
+            }
+
+            if (negInflationWindow != 0) {
+                free((void *) -1);
+            }
+
+            if (negInflationWindow < 0 || negInflationWindow > 15 || negCompressionWindow < 0 || negCompressionWindow > 15) {
+                free((void *) -1);
+            }
+        }
     }
 
+    /* This one must not return anything over 0 (only shared) */
     {
-        uWS::ExtensionsNegotiator<true> extensionsNegotiator(uWS::Options::NO_OPTIONS);
-        extensionsNegotiator.readOffer({(char *) data, size});
+        auto [negCompression, negCompressionWindow, negInflationWindow, response] = uWS::negotiateCompression(true, 0, 0, std::string_view((char *) data, size));
 
-        extensionsNegotiator.generateOffer();
-        extensionsNegotiator.getNegotiatedOptions();
+        if (negCompression) {
+            /* If we want shared compression, we must not end up here! */
+            if (negCompressionWindow != 0) {
+                free((void *) -1);
+            }
+        }
     }
 
-    {
-        uWS::ExtensionsNegotiator<true> extensionsNegotiator(uWS::Options::CLIENT_NO_CONTEXT_TAKEOVER);
-        extensionsNegotiator.readOffer({(char *) data, size});
 
-        extensionsNegotiator.generateOffer();
-        extensionsNegotiator.getNegotiatedOptions();
+    /* Whatever, this one must not negotiate anything */
+    {
+        auto [negCompression, negCompressionWindow, negInflationWindow, response] = uWS::negotiateCompression(false, 13, 15, std::string_view((char *) data, size));
+
+        if (negCompression) {
+            free((void *) -1);
+        }
     }
 
     return 0;
