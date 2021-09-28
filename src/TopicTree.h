814a7ee53e01b015a3f3e28e1ab3cf8752351bbe
Alex Hultman <alexhultman@gmail.com>
2021-09-28 23:23:00 +0200

    Clear outgoingMessages on last drained subscriber

--- src/TopicTree.h
+++ src/TopicTree.h
@@ -252,6 +252,11 @@ public:
             /* This one always resets needsDrainage before it calls any cb's.
              * Otherwise we would stackoverflow when sending after publish but before drain. */
             drainImpl(s);
+            
+            /* If we drained last subscriber, also clear outgoingMessages */
+            if (!drainableSubscribers) {
+                outgoingMessages.clear();
+            }
         }
     }
 
