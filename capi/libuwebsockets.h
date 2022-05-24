fd78f2960ac3c8ac529a11f115ba824db7e60c09
cirospaciari <ciro.spaciari@gmail.com>
2022-05-24 13:15:21 -0300

    added shared library in makefile for python tests

--- capi/libuwebsockets.h
+++ capi/libuwebsockets.h
@@ -103,7 +103,6 @@ extern "C"
         bool sendPingsAutomatically;
         /* Maximum socket lifetime in seconds before forced closure (defaults to disabled) */
         unsigned short maxLifetime;
-
         uws_websocket_upgrade_handler upgrade;
         uws_websocket_handler open;
         uws_websocket_message_handler message;
