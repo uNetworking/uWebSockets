6f7d8657a432cd10eadf0f5240841881b3c33516
Alex Hultman <alexhultman@gmail.com>
2023-04-30 23:20:54 +0200

    Add more query tests

--- tests/Query.cpp
+++ tests/Query.cpp
@@ -16,5 +16,27 @@ int main() {
         assert(uWS::getDecodedQueryValue("test2", (char *) buf.data()) == "someValue");
     }
 
+    {
+        std::string buf = "?Kest1=&test2=someValue";
+        assert(uWS::getDecodedQueryValue("test2", (char *) buf.data()) == "someValue");
+    }
+
+    {
+        std::string buf = "?Test1=&Kest2=some";
+        assert(uWS::getDecodedQueryValue("Test1", (char *) buf.data()) == "");
+        assert(uWS::getDecodedQueryValue("Kest2", (char *) buf.data()) == "some");
+    }
+
+    {
+        std::string buf = "?Test1=&Kest2=some";
+        assert(uWS::getDecodedQueryValue("Test1", (char *) buf.data()).data() != nullptr);
+        assert(uWS::getDecodedQueryValue("sdfsdf", (char *) buf.data()).data() == nullptr);
+    }
+
+    {
+        std::string buf = "?Kest1=&test2=some%20Value";
+        assert(uWS::getDecodedQueryValue("test2", (char *) buf.data()) == "some Value");
+    }
+
     return 0;
 }
