d5ec941fd8d29f3df6afbc73164d3c8bf0f49079
Alex Hultman <alexhultman@gmail.com>
2023-05-28 11:51:51 +0200

    Move http error pages to separate file

--- src/HttpParser.h
+++ src/HttpParser.h
@@ -33,23 +33,9 @@
 #include "BloomFilter.h"
 #include "ProxyParser.h"
 #include "QueryParser.h"
+#include "HttpErrors.h"
 
 namespace uWS {
-
-/* Possible errors from http parsing */
-enum HttpError {
-    HTTP_ERROR_505_HTTP_VERSION_NOT_SUPPORTED = 1,
-    HTTP_ERROR_431_REQUEST_HEADER_FIELDS_TOO_LARGE = 2,
-    HTTP_ERROR_400_BAD_REQUEST = 3
-};
-
-/* Returned parser errors match this LUT. */
-static const std::string_view httpErrorResponses[] = {
-    "", /* Zeroth place is no error so don't use it */
-    "HTTP/1.1 505 HTTP Version Not Supported\r\n\r\n<h1>HTTP Version Not Supported</h1><p>This server does not support HTTP/1.0.</p><hr><i>uWebSockets/20 Server</i>",
-    "HTTP/1.1 431 Request Header Fields Too Large\r\n\r\n<h1>Request Header Fields Too Large</h1><hr><i>uWebSockets/20 Server</i>",
-    "HTTP/1.1 400 Bad Request\r\n\r\n<h1>Bad Request</h1><hr><i>uWebSockets/20 Server</i>"
-};
 
 /* We require at least this much post padding */
 static const unsigned int MINIMUM_HTTP_POST_PADDING = 32;
