bd1c41a06b7e1dcdec5836c297629437ab218d2b
Alex Hultman <alexhultman@gmail.com>
2023-07-15 19:39:23 +0200

    Add default 404 error page

--- src/HttpErrors.h
+++ src/HttpErrors.h
@@ -25,7 +25,8 @@ namespace uWS {
 enum HttpError {
     HTTP_ERROR_505_HTTP_VERSION_NOT_SUPPORTED = 1,
     HTTP_ERROR_431_REQUEST_HEADER_FIELDS_TOO_LARGE = 2,
-    HTTP_ERROR_400_BAD_REQUEST = 3
+    HTTP_ERROR_400_BAD_REQUEST = 3,
+    HTTP_ERROR_404_FILE_NOT_FOUND = 4
 };
 
 /* Returned parser errors match this LUT. */
@@ -33,7 +34,8 @@ static const std::string_view httpErrorResponses[] = {
     "", /* Zeroth place is no error so don't use it */
     "HTTP/1.1 505 HTTP Version Not Supported\r\n\r\n<h1>HTTP Version Not Supported</h1><p>This server does not support HTTP/1.0.</p><hr><i>uWebSockets/20 Server</i>",
     "HTTP/1.1 431 Request Header Fields Too Large\r\n\r\n<h1>Request Header Fields Too Large</h1><hr><i>uWebSockets/20 Server</i>",
-    "HTTP/1.1 400 Bad Request\r\n\r\n<h1>Bad Request</h1><hr><i>uWebSockets/20 Server</i>"
+    "HTTP/1.1 400 Bad Request\r\n\r\n<h1>Bad Request</h1><hr><i>uWebSockets/20 Server</i>",
+    "HTTP/1.1 404 File Not Found\r\n\r\n<h1>File Not Found</h1><hr><i>uWebSockets/20 Server</i>"
 };
 }
 
