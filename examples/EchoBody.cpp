85b1bda3ef601392ba5e91e0e3803f5e197e9399
Alex Hultman <alexhultman@gmail.com>
2024-10-27 22:37:15 +0100

    Use new EchoBody example as compliance subject

---
+++ examples/EchoBody.cpp
@@ -0,0 +1,39 @@
+#include "App.h"
+
+/* Takes any method and echoes back the sent body. Can be used to test compliance of HTTP spec. */
+/* This example is also a good benchmark for body echoing. */
+
+int main() {
+
+	uWS::App().get("/*", [](auto *res, auto */*req*/) {
+		/* Technically the any route could be used likewise, but GET is optimized like this */
+		res->end();
+	}).any("/*", [](auto *res, auto */*req*/) {
+		std::unique_ptr<std::string> buffer;
+		res->onData([res, buffer = std::move(buffer)](std::string_view chunk, bool isFin) mutable {
+			if (isFin) [[likely]] {
+				if (buffer.get()) [[unlikely]] {
+					buffer->append(chunk);
+					res->end(*buffer);
+				} else {
+					res->end(chunk);
+				}
+			} else {
+				if (!buffer.get()) {
+					buffer = std::make_unique<std::string>(chunk);
+				} else {
+					buffer->append(chunk);
+				}
+			}
+		});
+
+		/* In this particular case we actually don't need to know this, as we only rely on RAII above. */
+		res->onAborted([]() {});
+	}).listen(3000, [](auto *listen_socket) {
+	    if (listen_socket) {
+			std::cerr << "Listening on port " << 3000 << std::endl;
+	    }
+	}).run();
+
+	std::cout << "Failed to listen on port 3000" << std::endl;
+}
