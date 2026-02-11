9054bfba1516aa53cd8c87ee996d480fe5619d54
uNetworkingAB <110806833+uNetworkingAB@users.noreply.github.com>
2026-02-11 04:01:44 +0100

    Update Docker image reference in server-test.js

--- autobahn/server-test.js
+++ autobahn/server-test.js
@@ -5,7 +5,7 @@ import { $ } from "https://deno.land/x/dax/mod.ts";
 const pwd = new URL(".", import.meta.url).pathname;
 
 const AUTOBAHN_TESTSUITE_DOCKER =
-  "crossbario/autobahn-testsuite:0.8.2@sha256:5d4ba3aa7d6ab2fdbf6606f3f4ecbe4b66f205ce1cbc176d6cdf650157e52242";
+  "crossbario/autobahn-testsuite:0.8.2";
 
 const server = $`uWebSockets/EchoServer`.spawn();
 await $`docker run --name fuzzingserver	-v ${pwd}/fuzzingclient.json:/fuzzingclient.json:ro	-v ${pwd}/reports:/reports -p 9001:9001	--net=host --rm	${AUTOBAHN_TESTSUITE_DOCKER} wstest -m fuzzingclient -s fuzzingclient.json`.cwd(pwd);
