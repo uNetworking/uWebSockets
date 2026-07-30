2b12665aed37dfb435dfbb27acc6c294e764b2e9
GetThatCookie <NimmenKeks@gmx.de>
2026-07-30 22:09:04 +0200

    Separate write hint from next length

--- src/AsyncSocket.h
+++ src/AsyncSocket.h
@@ -245,6 +245,7 @@ protected:
      * Returns pair of bytes written (anywhere) and whether or not this call resulted in the polling for
      * writable (or we are in a state that implies polling for writable). */
     std::pair<int, bool> write(const char *src, int length, bool optionally = false, int nextLength = 0) {
+    std::pair<int, bool> write(const char *src, int length, bool optionally = false, int nextLength = 0, bool hasMore = false) {
         /* Fake success if closed, simple fix to allow uncork of closed socket to succeed */
         if (us_socket_is_closed(SSL, (us_socket_t *) this)) {
             return {length, false};
@@ -257,6 +258,7 @@ protected:
         if (asyncSocketData->buffer.length()) {
             /* Write off as much as we can */
             int written = us_socket_write(SSL, (us_socket_t *) this, asyncSocketData->buffer.data(), (int) asyncSocketData->buffer.length(), /*nextLength != 0 | */length);
+            int written = us_socket_write(SSL, (us_socket_t *) this, asyncSocketData->buffer.data(), (int) asyncSocketData->buffer.length(), length || hasMore);
 
             /* On failure return, otherwise continue down the function */
             if ((unsigned int) written < asyncSocketData->buffer.length()) {
@@ -305,6 +307,7 @@ protected:
             } else {
                 /* We are not corked */
                 int written = us_socket_write(SSL, (us_socket_t *) this, src, length, nextLength != 0);
+                int written = us_socket_write(SSL, (us_socket_t *) this, src, length, nextLength != 0 || hasMore);
 
                 /* Did we fail? */
                 if (written < length) {
@@ -408,6 +411,7 @@ protected:
             if (loopData->corkOffset) {
                 /* Corked data is already accounted for via its write call */
                 bool failed = write(loopData->corkBuffer, (int) loopData->corkOffset, false, length ? length : (int) hasMore).second;
+                bool failed = write(loopData->corkBuffer, (int) loopData->corkOffset, false, length, hasMore).second;
                 loopData->corkOffset = 0;
 
                 if (failed) {
